d1acdf618b6f93573bd0dd7b2a1f2cf7c275debe
Yusuke Sasaki <ubnt.intrepid@gmail.com>
2025-12-05 16:31:54 +0900

    add wezterm.lua to WIP

---
+++ _wip/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Gruvbox light, medium (base16)'
+config.font_size = 14
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+
+return config
